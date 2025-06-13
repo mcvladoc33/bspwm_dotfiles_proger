1259c638112d2148451dd5a5feb50ed1f078c847
mcvladoc33 <48551124+mcvladoc33@users.noreply.github.com>
2025-06-13 17:22:54 +0300

    Update startup.sh

--- Builder/assets/startup.sh
+++ Builder/assets/startup.sh
@@ -6,9 +6,6 @@ default="\033[1;0m"
 
 clear
 
-printf "${cb} ┌─┐┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐  ┌┬┐┌─┐┌┬┐┌─┐┬┬  ┌─┐┌─┐\n"
-printf "${cb} ┌─┘├─┘├┬┘│ ││ ┬├┤ ├┬┘   │││ │ │ ├┤ ││  ├┤ └─┐\n"
-printf "${cb} └─┘┴  ┴└─└─┘└─┘└─┘┴└─  ─┴┘└─┘ ┴ └  ┴┴─┘└─┘└─┘\n\n"
+printf "${cb} Advel_dotfiles\n\n"
 printf "${cg} Version: 1.0\n"
-printf "${cg} Youtube: https://youtube.com/@zproger"
 printf "${default}\n\n"
