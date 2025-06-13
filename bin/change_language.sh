29877d2c5f63883d61c1c1a60060bfc36629b2f4
mcvladoc33 <48551124+mcvladoc33@users.noreply.github.com>
2025-06-13 17:28:19 +0300

    Update change_language.sh

--- bin/change_language.sh
+++ bin/change_language.sh
@@ -3,8 +3,8 @@
 CURRENT_LAYOUT=$(setxkbmap -query | awk -F : 'NR==3{print $2}' | sed 's/ //g')
 
 if [ "$CURRENT_LAYOUT" = "us" ]; then
-    setxkbmap "ru"
-	notify-send "Lang: RU" -t 700
+    setxkbmap "ua"
+	notify-send "Lang: UA" -t 700
 else
     setxkbmap "us"
     notify-send "Lang: US" -t 700
